0fcfc7216b0fb7d090b6f4d4e6de69d658882a9b
Leo Shen <rovingwind93@gmail.com>
2026-01-12 10:14:22 +0800

    update

---
+++ EditEnvironmentVaraibles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace EditEnvironmentVaraibles;
+
+internal class Program {
+  static void Main(/*string[] args*/) {
+    SV_Process.RunAsAdmin("rundll32.exe", "sysdm.cpl,EditEnvironmentVariables");
+  }
+}
+
+public class SV_Process {
+  public static void RunAsAdmin(string fileName, string? arguments = null) {
+    var processInfo = new ProcessStartInfo {
+      FileName = fileName,
+      Arguments = arguments,
+      // 以管理員身份運行
+      Verb = "runas",
+      // 必須設為 true 才能使用 "runas"
+      UseShellExecute = true
+    };
+
+    Process.Start(processInfo);
+  }
+}
